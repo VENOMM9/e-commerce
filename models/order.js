a1c8a81304c710df3f337114559ef2c4e3501978
Victor Nkwuo <vobinn3@gmail.com>
2023-12-13 22:20:16 +0300

    first coomit

---
+++ models/order.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const shortid = require("shortid");
+const bcrypt = require("bcrypt");
+
+
+const schema = mongoose.Schema;
+const ObjectId = schema.ObjectId;
+
+const orderSchema = new schema({
+ _id: {
+        type: String,
+     default: shortid.generate
+    },
+    name: { type: String, required: true },
+    state:{ type: String, required: false, enum : ['pending','completed'],default: 'pending'
+    },
+    user_id:{type:mongoose.Schema.Types.String,
+        ref:"users"
+        }
+    
+
+})
+
+
+
+const taskModel = mongoose.model("tasks", orderSchema);
+module.exports = orderModel;
